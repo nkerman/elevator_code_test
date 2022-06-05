c07d1d9ec3346c59215ff1e49f0a2eeb30872a4a
Nathaniel Kerman <NKerman@STScI.edu>
2022-06-05 11:54:21 -0400

    Add method for converting to readable time e.g., If a series of elevator moves will take 11143 seconds, this function will convert that to the more readable '3 hours, 5 minutes, 43 seconds.'

--- elevator.py
+++ elevator.py
@@ -99,3 +99,24 @@ e.travel_through_floor_list([1,2,3])
 print(e.report)
 
 # %%
+def readable_time_delta(num_seconds: int):
+    """Converts integer number of seconds into a string which is more familiar/interpretable to humans. Only clarifies to the level of hours.
+    Args:
+        num_seconds (int): Number of seconds to convert into a string. Floats work as well, but suggested/type-hinted value is an integer, which works with expected number of seconds for integer floors and fractional speed.
+    """
+    if num_seconds < 60: # seconds range
+        return str(num_seconds)
+    elif num_seconds < 3600: # minutes range
+        s_component = num_seconds % 60
+        m_component = int(num_seconds / 60) # use int to round *down* to nearest int
+        return f"{m_component} minutes {s_component} seconds"
+    elif num_seconds < 24*3600: # hours range
+        s_component = num_seconds % 60
+        m_component = int((num_seconds % (3600)) / 60)
+        h_component = int(num_seconds / 3600)
+        return f"{h_component} hours {m_component} minutes {s_component:.3f} seconds"
+
+# Example func call for prototyping - remove before release!
+b = (3.5*(60**2))+43*60+12.3
+readable_time_delta(b)
+# %%
