2bd115632c75e42308b600dd4604e2fccdfb2ba2
Nathaniel Kerman <NKerman@STScI.edu>
2022-06-06 09:29:05 -0400

    Work on documentation; rename height vars * variables like "current_height" didn't match the convention of next_floor, etc. Renamed them to e.g., current_floor. "building_height" unchanged.

--- elevator.py
+++ elevator.py
@@ -16,25 +16,21 @@ from numpy import diff
 from itertools import groupby
 # %%
 def readable_time_delta(num_seconds: int):
-    """Converts integer number of seconds into a string which is more familiar/interpretable to humans. Only clarifies to the level of hours.
+    """Converts integer number of seconds into a string which is more familiar/interpretable to humans. Only clarifies to the level of hours. If time is negative, just returns negative number of seconds (as str).
     Args:
         num_seconds (int): Number of seconds to convert into a string. Floats work as well, but suggested/type-hinted value is an integer, which works with expected number of seconds for integer floors and fractional speed.
     """
     if num_seconds < 60: # seconds range
-        return str(num_seconds)
+        return f"{num_seconds:.1f} seconds"
     elif num_seconds < 3600: # minutes range
         s_component = num_seconds % 60
         m_component = int(num_seconds / 60) # use int to round *down* to nearest int
-        return f"{m_component} minutes {s_component} seconds"
-    elif num_seconds < 24*3600: # hours range
+        return f"{m_component} minutes {s_component:.1f} seconds"
+    else: # hours and longer range
         s_component = num_seconds % 60
         m_component = int((num_seconds % (3600)) / 60)
         h_component = int(num_seconds / 3600)
         return f"{h_component} hours {m_component} minutes {s_component:.1f} seconds"
-
-# Example func call for prototyping - remove before release!
-b = (3.5*(60**2))+43*60+12.3
-readable_time_delta(b)
 # %%
 class Elevator:
     """An object to simulate an elevator starting on floor X and moving through a building of height Z.
@@ -43,8 +39,8 @@ class Elevator:
         Z (int): Height of the building in floors. Maximum height of the elevator.
     """
     def __init__(self, X: int, Z: int, speed: float=0.1, name: str="Elevator", building_name: str="a building") -> None:
-        self.start_height = X
-        self.current_height = self.start_height
+        self.start_floor = X
+        self.current_floor = self.start_floor
         self.building_height = Z
         self.speed = speed
         self.name = name
@@ -55,13 +51,12 @@ class Elevator:
         self.floor_history = []
         self.floor_future = []
         
-        assert self.start_height <= self.building_height, "The elevator may not start higher than the top floor."
+        assert self.start_floor <= self.building_height, "The elevator may not start higher than the top floor."
     
-    # Return None for now; essentially pass while developing
     @property
     def dist_to_next(self):
         if self.next_floor:
-            return self.next_floor - self.current_height
+            return self.next_floor - self.current_floor
         else:
             return None
     @property
@@ -72,6 +67,10 @@ class Elevator:
             return None
     @property
     def direction_to_next(self):
+        """Determines if the next floor is up, down or the same floor, and returns the best word for the report
+        Returns:
+            str: word to use in the report - i.e. 'up' in the phrase '...10 floors up...'
+        """
         if self.next_floor:
             if self.dist_to_next > 0:
                 return "up"
@@ -87,7 +86,7 @@ class Elevator:
         Returns: 
             int: Total integrated distance the elevator will take to get to the final floor in its list.
         """
-        all_remaining_distances = diff([self.current_height]+self.floor_future)
+        all_remaining_distances = diff([self.current_floor]+self.floor_future)
         return sum(abs(all_remaining_distances))
     @property
     def sum_time_to_final(self):
@@ -103,16 +102,19 @@ class Elevator:
         Returns:
             str: String representation of the summary report
         """
-        report_string = "\n### REPORT ###\n" + f"{self.name} is currently on floor {self.current_height} of {self.building_name} with {self.building_height} floors." 
+        report_string = "\n### REPORT ###\n" + f"{self.name} is currently on floor {self.current_floor} of {self.building_name} with {self.building_height} floors." 
         # If it's not done moving through the inputs, add this section of text.
         if self.next_floor:
-            report_string += f" The next floor ({self.next_floor}) is {abs(self.dist_to_next)} floors {self.direction_to_next} (ETA = {readable_time_delta(self.time_to_next)} seconds). The final commanded floor ({self.final_floor}) is {self.sum_dist_to_final} total floors away (ETA = {readable_time_delta(self.sum_time_to_final)} seconds)."
+            report_string += f" The next floor ({self.next_floor}) is {abs(self.dist_to_next)} floors {self.direction_to_next} (ETA = {readable_time_delta(self.time_to_next)}). The final commanded floor ({self.final_floor}) is {self.sum_dist_to_final} total floors away (ETA = {readable_time_delta(self.sum_time_to_final)})."
+        elif not self.next_floor:
+            report_string += "\nThis is the final commanded floor."
         return report_string
     
     def __repr__(self) -> str:
+        """Define a representation string for repr() function"""
         return str(self.report)
     
-    def travel_through_floor_list(self, floor_list, verbosity=2):
+    def travel_through_floor_list(self, floor_list: list, verbosity: int=2):
         # Remove subsequent duplicates in the floor_list; it's not meaningful to go from floor n to the same floor n.
         floor_list = [floor for floor, _group in groupby(floor_list)]
         # Set the floor_list to be the user-specified inputs list. 
@@ -127,13 +129,18 @@ class Elevator:
             self.next_floor = floor
             if verbosity > 1:
                 print(self.report)
-            # Keep track of where elevator is (current_height), has been (floor_history), and still needs to go (floor_future).
-            self.current_height = floor
+            # Keep track of where elevator is (current_floor), has been (floor_history), and still needs to go (floor_future).
+            self.current_floor = floor
             self.floor_history.append(floor)
             self.floor_future = self.floor_future[1:]
-        # At the end, set next_floor to None
+        # At the end of the list, set next_floor to None
         self.next_floor = None
+        if verbosity > 0:
+            print(self.report)
 
+# %%
+
+"""# %%
 #####
 # Example instance for prototyping - remove before release!
 e = Elevator(10,10, name="The Great Glass Elevator", building_name="The Chocolate Factory")
@@ -141,4 +148,13 @@ e = Elevator(10,10, name="The Great Glass Elevator", building_name="The Chocolat
 e.travel_through_floor_list([1,1,1,3,3,5,10,2])
 print(e.report)
 
+
+# Example func call for prototyping - remove before release!
+b = (3.5*(60**2))+43*60+12.3
+readable_time_delta(b)"""
+# %%
+e = Elevator(10,10, name="The Great Glass Elevator", building_name="The Chocolate Factory")
+# print(e.report)
+e.travel_through_floor_list([1,1,1,3,3,5,10,2], verbosity=1)
+e.travel_through_floor_list((1,2,3))
 # %%
