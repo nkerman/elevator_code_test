b9334a7c6a3dbd2b9a2a0a9f8165531d3211fb06
Nathaniel Kerman <NKerman@STScI.edu>
2022-06-06 23:48:16 -0400

    add more travel to example

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Title: example.py
 Author: Nat Kerman (nathaniel.kerman@gmail.com)
@@ -14,6 +14,8 @@ print(el)
 print("\n\n###### Now let's go for a ride! ######\n")
 # Travel through a list of floors
 el.travel_through_floor_list([1,20,3,9,100], verbosity=2)
+# You can then send the same object to more floors
+el.travel_through_floor_list([0,11], verbosity=2)
 # %%
 # You can also read the history of the Elevator - where it's been, how long it's traveled in space and time, etc
 print(f"The elevator named {el.name} has traveled {el.odometer} total floors in {readable_time_delta(el.time_elapsed)} seconds. Here's where it has been so far: {el.floor_history}.")
